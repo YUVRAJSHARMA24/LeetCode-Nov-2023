6ce862e150b7198ffbfea5e9f7e542761bcbd4ed
Yuvraj Sharma <yuvraj.june2002@gmail.com>
2023-11-06 17:39:47 +0530

    Create 1845-Seat-Reservation-Manager.cpp

---
+++ 1845-Seat-Reservation-Manager.cpp
@@ -0,0 +1,18 @@
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    SeatManager(int n) {
+        for(int i = 1; i<=n; i++)
+            pq.push(i);
+    }
+    
+    int reserve() {
+        int res = pq.top();
+        pq.pop();
+        return res;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
+};
